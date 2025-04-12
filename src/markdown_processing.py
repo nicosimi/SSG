4da6e24c3de4a5ac635e796f104ba3d80c6049d0
nicosimi <nicosimi993@gmail.com>
2025-04-12 23:32:36 +0900

    untested block to blocktype

--- src/markdown_processing.py
+++ src/markdown_processing.py
@@ -1,4 +1,13 @@
+from enum import Enum
+import re
 
+class BlockType(Enum):
+    PARAGRAPH = "paragraph"
+    HEADING = "heading"
+    CODE = "code"
+    QUOTE = "quote"
+    UNORDERED_LIST = "list_unordered"
+    ORDERED_LIST = "list_ordered"
 
 def markdown_to_blocks(md:str)->list[str]:
     if md is None: return []
@@ -8,3 +17,33 @@ def markdown_to_blocks(md:str)->list[str]:
         filter(lambda x: len(x) > 0, sections)
     )
     return sections
+
+def is_blocktype(lines:list[str], regex:str, ordered:bool) ->bool:
+    pattern = re.Pattern(regex)
+    if ordered:
+        i = 1
+        for line in lines:
+            if not pattern.match(line): return False
+            if int(line[0]) != i: return False
+            i+=1
+    else:
+        for line in lines:
+            if not pattern.match(line): return False
+    return True
+
+def block_to_blocktype(block:str)->BlockType:
+    pattern = re.Pattern("^\#{1,6}")
+    if pattern.match(block):
+        return BlockType.HEADING
+    pattern = re.Pattern("\`{3}")
+    if pattern.match(block) and pattern.match(block,(len(block)-3)):
+        return BlockType.CODE
+    lines = block.split("\n")
+    if is_blocktype(lines, "^\>", False):
+        return BlockType.QUOTE
+    if is_blocktype(lines, "^\- ", False):
+        return BlockType.UNORDERED_LIST
+    if is_blocktype(lines, "^\d\. ", True):
+        return BlockType.ORDERED_LIST
+    return BlockType.PARAGRAPH
+    
